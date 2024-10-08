43583d0d8a18b526ec871eba77cc612e796bacd1
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2024-10-08 17:09:17 +0500

    aNIMATION

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
-
+using System.Collections;
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody rb;
+    private Animator an;
+    [SerializeField] private int spd;
+    private Vector3 v;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        an = GetComponent<Animator>();
+        an.CrossFade("Idle",1);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        v.Set(Input.GetAxisRaw("Horizontal")*spd,rb.position.y,rb.position.z);
+        rb.linearVelocity = v;
+        an.SetBool("iswak",Input.GetAxisRaw("Horizontal") != 0);
     }
 }
