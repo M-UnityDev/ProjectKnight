786b03ffa6b42ceba8f6a4cb4190bd81d9441739
M-Unity <mihailsheshukov10@gmail.com>
2024-10-09 20:19:35 +0500

    Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,7 +5,11 @@ public class Movement : MonoBehaviour
     private Rigidbody rb;
     private Animator an;
     [SerializeField] private int spd;
+    [SerializeField] private GameObject rotob;
+    private Vector3 newpos;
+    private bool islft;
     private Vector3 v;
+    float zov = 0;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -14,8 +18,20 @@ public class Movement : MonoBehaviour
     }
     private void Update()
     {
-        v.Set(Input.GetAxisRaw("Horizontal")*spd,rb.position.y,rb.position.z);
+        float h = Input.GetAxisRaw("Horizontal");
+        v.Set(h*spd,rb.position.y,rb.position.z);
         rb.linearVelocity = v;
         an.SetBool("iswak",Input.GetAxisRaw("Horizontal") != 0);
+        
+        if (h > 0)
+            zov = 0;
+        else if (h < 0)
+            zov = 180;
+        newpos.Set(rotob.transform.localEulerAngles.x, zov, rotob.transform.localEulerAngles.z);
+
+    }
+    private void FixedUpdate()
+    {
+        rotob.transform.localEulerAngles = Vector3.Lerp(rotob.transform.localEulerAngles, newpos,Time.deltaTime * 5);
     }
 }
